b7b44acee8f654f047f707914c2f3730bdf4b8fe
Dmitriy Fishman <fishman.code@gmail.com>
2024-08-31 08:05:27 +0300

    Added Wi-Fi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = ""; //Enter SSID
+const char* password = ""; //Enter Password
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+     delay(1000);
+     Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  Serial.print("The IP Address of ESP8266 Module is: ");
+  Serial.print(WiFi.localIP());// Print the IP address
 }
 
 void loop() {
